38ee02588c518c69421a2e8cfb4ed47f97173611
nate-dean <natedeann@protonmail.com>
2026-02-17 06:06:39 +1100

    New buttons assigned to new funcs, all working

--- script.js
+++ script.js
@@ -1,85 +1,24 @@
-const display = document.getElementById("display")
-function appendToDisplay(number){
-    display.value += number;
+// const display = document.getElementById("display")
+
+// function clearDisplay(){
+//     display.value = " ";
+// };
+
+// function backspace(){
+//     display.value = display.value.slice(0, -1);
+// };
+
+let firstNumber = 0;
+let secondedNumber = 0;
+let operator = 0;
+let result = 0;
+let step = 0;
+
+
+function getNumber(num){
+console.log(num)
 };
 
-function clearDisplay(){
-    display.value = " ";
+function getOperator(op){
+console.log(op)
 };
-
-function backspace(){
-    display.value = display.value.slice(0, -1);
-};
-
-
-
-//Operators
-
-function add(num1, num2){
-   return parseFloat (num1) + parseFloat (num2);
-};
-
-function subtract(num1, num2){
-    return parseFloat (num1) - parseFloat (num2);
- };
-
- function multiply(num1, num2){
-    return parseFloat (num1) * parseFloat (num2);
- };
-
- function divide(num1, num2){
-    if(num2 === 0){
-        return "Err";
-    }
-    return parseFloat (num1) / parseFloat (num2);
- };
-
-
-function operate(operator, num1, num2){
-    switch(operator){
-        case '+':
-            return add(num1, num2);
-    break;
-        case '-':
-            return subtract(num1, num2);
-    break;
-        case '*':
-            return multiply(num1, num2);
-    break;
-        case '/':
-            return divide(num1, num2);
-    }
-}
-
-// let storedNum = "";
-// let clickedOperator = "";
-
-// currentOperhand.textContent = 0;
-// numberButton.forEach((number) => {
-//     number.addEventListener('click', function(){
-//         storedNum += number.value;
-//         previousOperhand.textContent = storedNum;
-//     })
-// });
-// operatorButton.forEach((operator => {
-//     operator.addEventListener('click', function(){
-//         num1 = storedNum; 
-//         clickedOperator = operator.textContent;
-//         previousOperhand.textContent = storedNum + clickedOperator;
-//         storedNum = "";
-//         console.log(num1 + storedNum);
-//         console.log(storedOperator);
-//         calculate();
-//     })
-// }));
-// function calculate(){
-//     const result = operate(clickedOperator, num1, num2, storedNum);
-//     currentOperhand.textContent = result;
-// }
-// equalButton.addEventListener('click', calculate);
-
-
-
-
-
-
