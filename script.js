46892d821d427053c9ba40db6aea7ce15537da3a
nate-dean <natedeann@protonmail.com>
2026-02-17 06:00:12 +1100

    Reassigned button funcs

--- script.js
+++ script.js
@@ -28,8 +28,56 @@ function subtract(num1, num2){
  };
 
  function divide(num1, num2){
+    if(num2 === 0){
+        return "Err";
+    }
     return parseFloat (num1) / parseFloat (num2);
  };
+
+
+function operate(operator, num1, num2){
+    switch(operator){
+        case '+':
+            return add(num1, num2);
+    break;
+        case '-':
+            return subtract(num1, num2);
+    break;
+        case '*':
+            return multiply(num1, num2);
+    break;
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+// let storedNum = "";
+// let clickedOperator = "";
+
+// currentOperhand.textContent = 0;
+// numberButton.forEach((number) => {
+//     number.addEventListener('click', function(){
+//         storedNum += number.value;
+//         previousOperhand.textContent = storedNum;
+//     })
+// });
+// operatorButton.forEach((operator => {
+//     operator.addEventListener('click', function(){
+//         num1 = storedNum; 
+//         clickedOperator = operator.textContent;
+//         previousOperhand.textContent = storedNum + clickedOperator;
+//         storedNum = "";
+//         console.log(num1 + storedNum);
+//         console.log(storedOperator);
+//         calculate();
+//     })
+// }));
+// function calculate(){
+//     const result = operate(clickedOperator, num1, num2, storedNum);
+//     currentOperhand.textContent = result;
+// }
+// equalButton.addEventListener('click', calculate);
+
 
 
 
